0637f18e23b2ed436aee241278d78ee728b49345
Brian Zhao <bzhao1991@gmail.com>
2021-10-28 22:49:55 -0400

    implements deep dive and modifies video details styles

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -12,6 +12,27 @@ export default function Comment(props) {
     const newYear = newDate.getFullYear();
     return `${newMonth}/${newDay}/${newYear}`;
   };
+
+  const deepDive = (date) => {
+    const daysPassed = Math.floor((Date.now() - date) / 86400000);
+    if (daysPassed < 1) {
+      return `less than 1 day ago`;
+    }
+    if (daysPassed < 2) {
+      return `1 day ago`;
+    }
+    if (daysPassed < 30) {
+      return `${daysPassed} days ago`;
+    }
+    if (daysPassed < 60) {
+      return `1 month ago`;
+    }
+    if (daysPassed < 365) {
+      return `${Math.floor(daysPassed / 30)} months ago`;
+    }
+    return `over 1 year ago`;
+  };
+
   return (
     <article className="comment">
       <div className="comment__profile-pic"></div>
@@ -19,7 +40,7 @@ export default function Comment(props) {
         <div className="comment__header">
           <h3 className="comment__user">{props.comment.name}</h3>
           <h4 className="comment__timestamp">
-            {formatDate(props.comment.timestamp)}
+            {deepDive(props.comment.timestamp)}
           </h4>
         </div>
         <p className="comment__comment">{props.comment.comment}</p>
--- src/components/VideoDetails/VideoDetails.js
+++ src/components/VideoDetails/VideoDetails.js
@@ -15,12 +15,32 @@ export default function VideoDetails(props) {
     return `${newMonth}/${newDay}/${newYear}`;
   };
 
+  const deepDive = (date) => {
+    const daysPassed = Math.floor((Date.now() - date) / 86400000);
+    if (daysPassed < 1) {
+      return `less than 1 day ago`;
+    }
+    if (daysPassed < 2) {
+      return `1 day ago`;
+    }
+    if (daysPassed < 30) {
+      return `${daysPassed} days ago`;
+    }
+    if (daysPassed < 60) {
+      return `1 month ago`;
+    }
+    if (daysPassed < 365) {
+      return `${Math.floor(daysPassed / 30)} months ago`;
+    }
+    return `over 1 year ago`;
+  };
+
   return (
     <article className="video-details">
       <div className="video-details__header">
         <h3 className="video-details__channel">By {props.channel}</h3>
         <h4 className="video-details__timestamp">
-          {formatDate(props.timestamp)}
+          {deepDive(props.timestamp)}
         </h4>
       </div>
       <div className="video-details__stats">
