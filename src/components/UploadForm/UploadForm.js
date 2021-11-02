90262574687d96c74fc2dca65547f8bbdca1f228
Brian Zhao <bzhao1991@gmail.com>
2021-11-01 20:19:55 -0400

    finishes style for uploads page and applies new style to a tags

--- src/components/UploadForm/UploadForm.js
+++ src/components/UploadForm/UploadForm.js
@@ -3,33 +3,46 @@ import Button from "../Button/Button";
 import upload from "../../assets/icons/upload.svg";
 import { Link } from "react-router-dom";
 import "./UploadForm.scss";
+import uploadThumb from "../../assets/images/upload.jpg";
 
-export default function UploadForm(props) {
+export default function UploadForm() {
   return (
     <form className="upload-form">
-      <label className="upload-form__thumbnail__label">
-        VIDEO THUMBNAIL
-        <img className="upload-form__thumbnail" alt="video thumbnail"></img>
-      </label>
-      <label className="upload-form__title__label">
-        TITLE YOUR VIDEO
-        <input
-          className="upload-form__title"
-          type="text"
-          placeholder="Add a title to your video"
-        ></input>
-      </label>
-      <label className="upload-form__description__label">
-        ADD A VIDEO DESCRIPTION
-        <textarea
-          className="upload-form__description"
-          placeholder="Add a description to your video"
-        ></textarea>
-      </label>
-      <Link to="/">
-        <Button icon={upload} text="PUBLISH" />
-      </Link>
-      <Link to="/">CANCEL</Link>
+      <article className="upload-form__input">
+        <label className="upload-form__thumbnail__label">
+          VIDEO THUMBNAIL
+          <img
+            className="upload-form__thumbnail"
+            src={uploadThumb}
+            alt="video thumbnail"
+          ></img>
+        </label>
+        <section className="upload-form__text">
+          <label className="upload-form__title__label">
+            TITLE YOUR VIDEO
+            <input
+              className="upload-form__title"
+              type="text"
+              placeholder="Add a title to your video"
+            ></input>
+          </label>
+          <label className="upload-form__description__label">
+            ADD A VIDEO DESCRIPTION
+            <textarea
+              className="upload-form__description"
+              placeholder="Add a description to your video"
+            ></textarea>
+          </label>
+        </section>
+      </article>
+      <section className="upload-form__buttons">
+        <Link to="/">
+          <Button icon={upload} text="PUBLISH" />
+        </Link>
+        <Link to="/">
+          <p className="upload-form__cancel">CANCEL</p>
+        </Link>
+      </section>
     </form>
   );
 }
--- src/pages/UploadPage/UploadPage.js
+++ src/pages/UploadPage/UploadPage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import UploadForm from "../../components/UploadForm/UploadForm";
 import VideoTitle from "../../components/VideoTitle/VideoTitle";
-
+import "./UploadPage.scss";
 export default function UploadPage() {
   return (
-    <div>
-      <VideoTitle text="Upload Video" />
-      <UploadForm />
-    </div>
+    <main className="upload-page">
+      <div className="upload-page__styling">
+        <VideoTitle text="Upload Video" />
+        <UploadForm />
+      </div>
+    </main>
   );
 }
